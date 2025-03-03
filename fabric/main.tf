b7a78dae97a5f895a85d8e6e26ab1e9b1d35647e
Dean Murray <129386407+fake-cloud-dm@users.noreply.github.com>
2025-03-03 13:12:57 +0000

    main.tf

--- fabric/main.tf
+++ fabric/main.tf
@@ -1,15 +1,15 @@
-resource "azurerm_powerbi_capacity" "fabric_capacity" {
+resource "azurerm_powerbi_embedded_capacity" "fabric_capacity" {
   name                = var.fabric_capacity_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.existing_rg ? data.azurerm_resource_group.rg[0].name : azurerm_resource_group.rg[0].name
   location            = var.location
   sku_name            = var.sku_name
   admin_users         = var.admin_users
 }
 
-resource "azurerm_powerbi_workspace" "fabric_workspace" {
+resource "azurerm_powerbi_embedded_workspace" "fabric_workspace" {
   name                = var.fabric_workspace_name
-  capacity_id         = azurerm_powerbi_capacity.fabric_capacity.id
-  resource_group_name = var.resource_group_name
+  capacity_id         = azurerm_powerbi_embedded_capacity.fabric_capacity.id
+  resource_group_name = var.existing_rg ? data.azurerm_resource_group.rg[0].name : azurerm_resource_group.rg[0].name
 }
 
 # Add resources for VNet, Private Link, etc., if needed
