2a49c1f8e3ff0385dec09c97d4ef414347524417
Dean Murray <129386407+fake-cloud-dm@users.noreply.github.com>
2025-03-03 13:27:11 +0000

    main

--- fabric/main.tf
+++ fabric/main.tf
@@ -15,10 +15,13 @@ resource "azurerm_fabric_capacity" "fabric_capacity" {
   }
 }
 
-resource "azurerm_powerbi_workspace" "fabric_workspace" {
-  name                = var.fabric_workspace_name
-  capacity_id         = azurerm_fabric_capacity.fabric_capacity.id
-  resource_group_name = var.existing_rg ? data.azurerm_resource_group.rg[0].name : azurerm_resource_group.rg[0].name
+resource "fabric_workspace" "example2" {
+  display_name = "example2"
+  description  = "Example Workspace 2"
+  capacity_id  = azurerm_fabric_capacity.fabric_capacity.id
+  identity = {
+    type = "SystemAssigned"
+  }
 }
 
 # Add resources for VNet, Private Link, etc., if needed
