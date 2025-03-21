91399c9d4874a3fc30a0328787cb99aae9393148
Dean Murray <129386407+fake-cloud-dm@users.noreply.github.com>
2025-03-21 14:32:28 +0000

    Update virtual_network.tf

--- fabric/virtual_network.tf
+++ fabric/virtual_network.tf
@@ -87,19 +87,19 @@ resource "azurerm_subnet_route_table_association" "rt_association_connectivity" 
 }
 
 
-# Fabric Virtual Network Gateway
-resource "fabric_gateway" "fabric_vnet_gateway" {
-  type                            = "VirtualNetwork"
-  display_name                    = "fabric-vnet-gw-prod-${var.location_short}-001"
-  inactivity_minutes_before_sleep = 30 # Adjust as needed
-  number_of_member_gateways       = 1
-  virtual_network_azure_resource = {
-    resource_group_name  = var.existing_rg ? data.azurerm_resource_group.rg[0].name : azurerm_resource_group.rg[0].name
-    virtual_network_name = azurerm_virtual_network.fabric_vnet.name
-    subnet_name          = azurerm_subnet.gateway_subnet.name
-    subscription_id      = var.subscription_id
-  }
-  capacity_id = data.fabric_capacity.capacity.id
+# # Fabric Virtual Network Gateway
+# resource "fabric_gateway" "fabric_vnet_gateway" {
+#   type                            = "VirtualNetwork"
+#   display_name                    = "fabric-vnet-gw-prod-${var.location_short}-001"
+#   inactivity_minutes_before_sleep = 30 # Adjust as needed
+#   number_of_member_gateways       = 1
+#   virtual_network_azure_resource = {
+#     resource_group_name  = var.existing_rg ? data.azurerm_resource_group.rg[0].name : azurerm_resource_group.rg[0].name
+#     virtual_network_name = azurerm_virtual_network.fabric_vnet.name
+#     subnet_name          = azurerm_subnet.gateway_subnet.name
+#     subscription_id      = var.subscription_id
+#   }
+#   capacity_id = data.fabric_capacity.capacity.id
 
-  depends_on = [azurerm_virtual_network.fabric_vnet, azurerm_subnet.gateway_subnet]
-}
+#   depends_on = [azurerm_virtual_network.fabric_vnet, azurerm_subnet.gateway_subnet]
+# }
