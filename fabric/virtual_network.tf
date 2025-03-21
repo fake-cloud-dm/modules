2f30710a281a40e84c98e8657bc92a336751e7e2
Dean Murray <129386407+fake-cloud-dm@users.noreply.github.com>
2025-03-21 13:23:41 +0000

    Update virtual_network.tf

--- fabric/virtual_network.tf
+++ fabric/virtual_network.tf
@@ -22,7 +22,7 @@ resource "azurerm_subnet" "gateway_subnet" {
   delegation {
     name = "Microsoft.PowerPlatformDelegation"
     service_delegation {
-      name = "Microsoft.PowerPlatform/vnetaccesslinks"
+      name = "Microsoft.ServiceFabricMesh/networks"
       actions = [
         "Microsoft.Network/virtualNetworks/subnets/join/action",
       ]
