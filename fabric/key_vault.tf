d9a69bd58c974a8cd97cb8ee625c3c3ed1cc801f
Dean Murray <129386407+fake-cloud-dm@users.noreply.github.com>
2025-03-26 21:09:56 +0000

    Update key_vault.tf

--- fabric/key_vault.tf
+++ fabric/key_vault.tf
@@ -53,3 +53,30 @@ resource "azurerm_key_vault_access_policy" "workspace_kv_access" {
 
   depends_on = [azurerm_key_vault.workspace_keyvault, fabric_workspace.workspaces]
 }
+
+resource "azurerm_key_vault_access_policy" "workspace_kv_access" {
+  for_each     = fabric_workspace.workspaces
+  key_vault_id = azurerm_key_vault.workspace_keyvault[each.key].id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azuread_group.ans_engineer_fabric_admin.object_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+  ]
+
+  certificate_permissions = [
+    "Get",
+    "List",
+    "Update",
+  ]
+
+  key_permissions = [
+    "Get",
+    "List",
+    "Update",
+  ]
+
+  depends_on = [azuread_group.ans_engineer_fabric_admin]
+}
